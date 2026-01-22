539b2a9fcdd6398ca0cdd2b279f8536fe6521785
irasodhi <irasodhi5@gmail.com>
2026-01-22 17:26:10 +0530

    done

---
+++ Demo.java
@@ -0,0 +1,31 @@
+// class student{
+//     public static void main(String[] ira) {
+//         System.out.println("welcome to chitkara");
+//     }
+// }
+
+
+//--------------------------------------------------------------------------------------------
+// class Student{
+// 	static {
+// 		System.out.println("i m from student class");
+// 	}
+// }
+// public class Demo {
+// 	static {
+// 		System.out.println("hi");
+// 	}
+// public static void main(String[] arg) {
+// 	Student st =new Student();
+// 	System.out.println("hello world");
+// }
+// }
+
+//-----------------------------------------------------------------------------------------
+
+// public class Demo{
+// 	static double d=5.6;
+// 	public static void main(String[] args){
+// 		System.out.println(d);
+// 	}
+// }
--- day1.java
+++
@@ -1,7 +0,0 @@
-class student{
-    public static void main(String[] ira) {
-        System.out.println("welcome to chitkara");
-    }
-}
-
-
