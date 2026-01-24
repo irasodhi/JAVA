54144495c13e799b2a2aef60aed0e61d4562e78f
irasodhi <irasodhi5@gmail.com>
2026-01-24 16:01:24 +0530

    done

---
+++ ifelse.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+// public class ifelse {
+
+//     public static void main(String[] args) {
+//       Scanner sc=new Scanner(System.in);  
+//       int age=sc.nextInt();
+//       if(age>=18){
+//         System.out.println("adult");
+//       }
+//       else{
+//         System.out.println("Not adult");
+//       }
+//       sc.close();
+//     }
+// }
+
+// public class ifelse {
+// public static void main(String[] args) {
+//     Scanner sc=new Scanner(System.in);
+//     int n=sc.nextInt();
+//     if(n%2==0){
+//         System.out.println("even");
+//     }
+//     else{
+//         System.out.println("odd");
+//     }
+//     sc.close();
+// }
+    
+// }
