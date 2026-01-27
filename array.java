afef3b42e549a35dc4cf462c54619109da61e87d
irasodhi <irasodhi5@gmail.com>
2026-01-27 18:21:01 +0530

    Done

---
+++ array.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int numb[]=new int[size];
+        for(int i=0;i<size;i++){
+            numb[i]=sc.nextInt();
+        }
+        for(int i=0;i<size;i++){
+            System.out.println(numb[i]);
+        }
+    sc.close();
+    }
+}
