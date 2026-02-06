58c308d5d8f7636146d222fb4185507c378e818e
irasodhi <irasodhi5@gmail.com>
2026-02-06 18:28:50 +0530

    done

---
+++ classnotes/Array.java
@@ -0,0 +1,34 @@
+public class Array {
+public static void main(String[] args) {
+//	int[] arr=new int[5];
+//	String[] name=new String[5];
+//	System.out.println(arr[1]);
+//	System.out.println(name[1]);
+//	int[] arr1= {1,2,3,4,5};
+//	System.out.println(arr1);
+//	arr[0]=1;
+//	arr[1]=2;
+//	arr[2]=3;
+//	arr[3]=4;
+//	arr[4]=5;
+//	int count=1;
+//	for(int i=0;i<arr.length;i++) {
+//		arr[i]=2*count;
+//		count++;
+//		System.out.println(arr[i]);
+//	}
+//	for(Integer e:arr) {
+//		System.out.println(e);
+//	}
+//	int[] numbers= {2,-9,0,5,12,-25,22,9,8,12};
+//	int sum=0;
+//	int n=numbers.length;
+//	for(int i=0;i<numbers.length;i++) {
+//		sum+=numbers[i];
+//	}
+//	System.out.println(sum);
+//	double avg=(double)sum/n;
+//	System.out.println(avg);
+	
+}
+}
