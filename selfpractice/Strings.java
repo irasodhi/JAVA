695a1f841b9bc34043ed3d4f80d4e4986d23e676
irasodhi <irasodhi5@gmail.com>
2026-03-20 00:42:27 +0530

    done

---
+++ selfpractice/Strings.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Strings{
+    public static void main(String[] args) {
+        // String name="Tony";
+        // String fullname="Tony Stark";
+        // String sentence="my name is tony stark";
+
+
+        // Scanner sc=new Scanner(System.in);
+        // String name=sc.nextLine();
+        // System.out.println("your name is"+name);
+
+        //concatenation
+        // String fname="tony";
+        // String lastname="stark";
+        // String fullname=fname+"&"+lastname;
+
+        // System.out.println(fullname);
+
+        // //charAt
+        // for(int i=0;i<fullname.length();i++){
+        //     System.out.println(fullname.charAt(i));
+        // }
+
+        // //compare
+        // String name1="Tony";
+        // String name2="Tony";
+        // //s1>s2
+        // //s1==s2
+        // //s1<s2
+        // if(name1.compareTo(name2)==0){
+        //     System.out.println("Strings are equal");
+        // }
+        // else{
+        //     System.out.println("{not equal");
+        // }
+        String sentence="my name is tony";
+        String name=sentence.substring(11,sentence.length());
+        
+
+    }
+}
