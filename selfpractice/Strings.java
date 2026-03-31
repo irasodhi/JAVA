bcc11097250ca43c38a9be338a6a225fa5e38b4f
irasodhi <irasodhi5@gmail.com>
2026-03-31 18:20:39 +0530

    done

--- selfpractice/Strings.java
+++ selfpractice/Strings.java
@@ -1,42 +1,95 @@
 import java.util.*;
+
+
+
+//IMMUTABLE
+
 public class Strings{
     public static void main(String[] args) {
-        // String name="Tony";
-        // String fullname="Tony Stark";
-        // String sentence="my name is tony stark";
+    //     // String name="Tony";
+    //     // String fullname="Tony Stark";
+    //     // String sentence="my name is tony stark";
 
 
-        // Scanner sc=new Scanner(System.in);
-        // String name=sc.nextLine();
-        // System.out.println("your name is"+name);
+    //     // Scanner sc=new Scanner(System.in);
+    //     // String name=sc.nextLine();
+    //     // System.out.println("your name is"+name);
 
-        //concatenation
-        // String fname="tony";
-        // String lastname="stark";
-        // String fullname=fname+"&"+lastname;
+    //     //concatenation
+    //     // String fname="tony";
+    //     // String lastname="stark";
+    //     // String fullname=fname+"&"+lastname;
 
-        // System.out.println(fullname);
+    //     // System.out.println(fullname);
 
-        // //charAt
-        // for(int i=0;i<fullname.length();i++){
-        //     System.out.println(fullname.charAt(i));
-        // }
+    //     // //charAt
+    //     // for(int i=0;i<fullname.length();i++){
+    //     //     System.out.println(fullname.charAt(i));
+    //     // }
 
-        // //compare
-        // String name1="Tony";
-        // String name2="Tony";
-        // //s1>s2
-        // //s1==s2
-        // //s1<s2
-        // if(name1.compareTo(name2)==0){
-        //     System.out.println("Strings are equal");
-        // }
-        // else{
-        //     System.out.println("{not equal");
-        // }
-        String sentence="my name is tony";
-        String name=sentence.substring(11,sentence.length());
+    //     // //compare
+    //     // String name1="Tony";
+    //     // String name2="Tony";
+    //     // //s1>s2
+    //     // //s1==s2
+    //     // //s1<s2
+    //     // if(name1.compareTo(name2)==0){
+    //     //     System.out.println("Strings are equal");
+    //     // }
+    //     // else{
+    //     //     System.out.println("{not equal");
+    //     // }
+    //     // String sentence="my name is tony";
+    //     // String name=sentence.substring(11,sentence.length());
         
+    //     StringBuilder sb=new StringBuilder("Tony");
+    //     System.out.println(sb);
+    //     //char at 0
+    //     System.out.println(sb.charAt(0));
+    //     //set char at indx 0
+    //     // sb.setCharAt(0,'p');
+    //     // System.out.println(sb);
+    //    sb.insert(2,'n');
+    //    System.out.println(sb);
+    //    sb.delete(2,3);
+    //    System.out.println(sb); 
 
-    }
+    //reverse a dtring
+    // Scanner sc=new Scanner(System.in);
+    // String name=sc.nextLine();
+    // for(int i=name.length()-1;i>=0;i--){
+    //     System.out.print(name.charAt(i));
+    // }
+
+
+
+//reverse words in sentence
+    // Scanner sc=new Scanner(System.in);
+    // String name=sc.nextLine();
+    // String[] words=name.split("\\s+");
+    // StringBuilder sb=new StringBuilder();
+  
+    // for(int i=words.length-1;i>=0;i--){
+    
+    //      sb.append(words[i]);
+    // sb.append(" ");
+    
+    // }
+    // System.out.println(sb.toString().trim());
+   
+
+//reverse each words
+
+//    Scanner sc=new Scanner(System.in);
+//     String name=sc.nextLine();
+//     String[] words=name.split("\\s+");
+//     StringBuilder result=new StringBuilder();
+//     for(String word:words){
+//     StringBuilder sb=new StringBuilder(word); 
+//         result.append(sb.reverse().toString());
+//         result.append(" ");
+//     }
+// System.out.println(result.toString().trim());
+//     }
+
 }
